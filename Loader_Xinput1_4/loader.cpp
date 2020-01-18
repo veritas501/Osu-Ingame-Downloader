78ce6d9bc378454ab91758260336a428bb46025d
veritas501 <hxzene@gmail.com>
2020-01-18 20:29:04 +0800

    update loader

--- Loader_Xinput1_4/loader.cpp
+++ Loader_Xinput1_4/loader.cpp
@@ -1,29 +1,41 @@
 #include "pch.h"
 #include <Windows.h>
 #include <io.h>
+#include <stdio.h>
 #include "loader.h"
 
 HMODULE hm = nullptr;
 
 int LoaderInitialize() {
+	char currentPath[MAX_PATH] = "";
+	char newVersionName[MAX_PATH + 20] = "";
+	char oldVersionName[MAX_PATH + 20] = "";
 	bool hasNewVersion;
 	bool hasOldVersion;
 
+	if (!GetModuleFileNameA(NULL, currentPath, MAX_PATH)) {
+		MessageBoxA(0, "Loader: Can't get current path", "Error", 0);
+		return 0;
+	}
+	strrchr(currentPath, '\\')[0] = 0;
+	snprintf(oldVersionName, MAX_PATH + 20, "%s\\IngameDL.dll", currentPath);
+	snprintf(newVersionName, MAX_PATH + 20, "%s\\IngameDL.new.dll", currentPath);
+
 	// update IngameDL module
-	hasNewVersion = !_access("IngameDL.new.dll", 0);
-	hasOldVersion = !_access("IngameDL.dll", 0);
+	hasNewVersion = !_access(newVersionName, 0);
+	hasOldVersion = !_access(oldVersionName, 0);
 	if (!hasNewVersion && !hasOldVersion) {
-		MessageBoxA(0, "Can't find IngameDL.dll", "Error", 0);
+		MessageBoxA(0, "Loader: Can't find IngameDL.dll", "Error", 0);
 		return 0;
 	}
 	if (hasNewVersion) {
 		if (hasOldVersion) {
-			if (!DeleteFileA("IngameDL.dll")) {
-				MessageBoxA(0, "Can't delete old IngameDL.dll", "Update", 0);
+			if (!DeleteFileA(oldVersionName)) {
+				MessageBoxA(0, "Loader: Can't delete old IngameDL.dll", "Update", 0);
 				return 1;
 			}
-			if (rename("IngameDL.new.dll", "IngameDL.dll")) {
-				MessageBoxA(0, "Can't rename IngameDL.new.dll to IngameDL.dll", "Update", 0);
+			if (rename(newVersionName, oldVersionName)) {
+				MessageBoxA(0, "Loader: Can't rename IngameDL.new.dll to IngameDL.dll", "Update", 0);
 				return 1;
 			}
 		}
@@ -34,9 +46,9 @@ int LoaderInitialize() {
 		return 0;
 	}
 	// load main module
-	hm = LoadLibraryA("IngameDL.dll");
+	hm = LoadLibraryA(oldVersionName);
 	if (!hm) {
-		MessageBoxA(0,"Can't load IngameDL.dll","Error",0);
+		MessageBoxA(0, "Loader: Can't load IngameDL.dll", "Error", 0);
 		return 1;
 	}
 	return 0;
